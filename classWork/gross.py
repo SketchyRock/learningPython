d8976685558a60bdb1b7d4f3551eed0415c40856
Enzo Hins <rock@Enzos-Laptop.local>
2025-09-11 10:10:17 -0400

    re-arranging

--- classWork/gross.py
+++
@@ -1,9 +0,0 @@
-"""Program recieves an input from the user and returns the grossed verison.
-
-Author: Enzo Hins
-Version: 20/8/2025
-"""
-
-num = int(input())
-gross = num * 144
-print(gross)
--- classWork/hello_world.py
+++
@@ -1,6 +0,0 @@
-"""This program prints hello world
-
-Author: Enzo Hins
-Version: 20/8/2025
-"""
-print('Hello, World!')
